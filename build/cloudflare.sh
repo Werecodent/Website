b5280d0acb53ddaffeefbbf491256ef4a83e39b9
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 14:28:37 -0600

    Correct call.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -4,8 +4,8 @@ mkdir -p artifacts/install
 curl -sSL https://dot.net/v1/dotnet-install.sh > artifacts/install/dotnet-install.sh
 chmod +x artifacts/install/dotnet-install.sh
 ./artifacts/install/dotnet-install.sh -c 10.0 -InstallDir artifacts/dotnet
-./artifacts/dotnet --version
 
+dotnet --version
 ./build/run.sh publish
 
 #$ARTIFACT_DIR/dotnet publish -c Release -o ./artifacts/releases
