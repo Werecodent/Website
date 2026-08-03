70c13aac72f623806307389b14e70b1473ebef8a
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 13:21:39 -0600

    Add cloudflare specific build script.

---
+++ build/cloudflare.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > ./artifacts/install/dotnet-install.sh
+chmod +x ./artifacts/install/dotnet-install.sh
+./artifacts/dotnet-install.sh -c 10.0 -InstallDir ./artifacts/dotnet
+./artifacts/dotnet --version
+./artifacts/dotnet publish -c Release -o ./artifacts/releases
