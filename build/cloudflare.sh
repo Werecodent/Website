25db2a7193ac9af693c64a4ac510835d2eede37c
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 13:34:40 -0600

    Better script?

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-mkdir -p ./artifacts/install
-curl -sSL https://dot.net/v1/dotnet-install.sh > artifacts/install/dotnet-install.sh
+
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+cd "$SCRIPT_DIR/.."
+
+ARTIFACT_DIR="SCRIPT_DIR/../artifacts"
+
+mkdir -p "$ARTIFACT_DIR/install"
+curl -sSL https://dot.net/v1/dotnet-install.sh > "$ARTIFACT_DIR/install/dotnet-install.sh"
 chmod +x ./artifacts/install/dotnet-install.sh
-./artifacts/dotnet-install.sh -c 10.0 -InstallDir ./artifacts/dotnet
-./artifacts/dotnet --version
-./artifacts/dotnet publish -c Release -o ./artifacts/releases
+$ARTIFACT_DIR/install/dotnet-install.sh -c 10.0 -InstallDir ./artifacts/dotnet
+$ARTIFACT_DIR/dotnet --version
+
+$SCRIPT_DIR/run.sh publish
+
+#$ARTIFACT_DIR/dotnet publish -c Release -o ./artifacts/releases
