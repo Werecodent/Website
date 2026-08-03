6b754ee16a76136864cf6e55b5865efe13397481
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 14:25:47 -0600

    Script tweaks.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-cd "$SCRIPT_DIR/.."
+mkdir -p artifacts/install
+curl -sSL https://dot.net/v1/dotnet-install.sh > artifacts/install/dotnet-install.sh
+chmod +x artifacts/install/dotnet-install.sh
+./artifacts/install/dotnet-install.sh -c 10.0 -InstallDir artifacts/dotnet
+./artifacts/dotnet --version
 
-ARTIFACT_DIR="SCRIPT_DIR/../artifacts"
-
-mkdir -p "$ARTIFACT_DIR/install"
-curl -sSL https://dot.net/v1/dotnet-install.sh > "$ARTIFACT_DIR/install/dotnet-install.sh"
-chmod +x ./artifacts/install/dotnet-install.sh
-$ARTIFACT_DIR/install/dotnet-install.sh -c 10.0 -InstallDir ./artifacts/dotnet
-$ARTIFACT_DIR/dotnet --version
-
-$SCRIPT_DIR/run.sh publish
+./build/run.sh publish
 
 #$ARTIFACT_DIR/dotnet publish -c Release -o ./artifacts/releases
