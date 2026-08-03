ca5957d1292fc2f67c6ca16eb69dfc17761f96b1
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 13:24:58 -0600

    Create install directory for cloudflare.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir ./artifacts/install
 curl -sSL https://dot.net/v1/dotnet-install.sh > ./artifacts/install/dotnet-install.sh
 chmod +x ./artifacts/install/dotnet-install.sh
 ./artifacts/dotnet-install.sh -c 10.0 -InstallDir ./artifacts/dotnet
