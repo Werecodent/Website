322e3c0fe4c519aaf336f09057e71acf6ddbf1b3
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 13:25:58 -0600

    Add recursive flag.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir ./artifacts/install
+mkdir -p ./artifacts/install
 curl -sSL https://dot.net/v1/dotnet-install.sh > ./artifacts/install/dotnet-install.sh
 chmod +x ./artifacts/install/dotnet-install.sh
 ./artifacts/dotnet-install.sh -c 10.0 -InstallDir ./artifacts/dotnet
