8e3928eda4dc919223ff3d3b9a739d40ec7407b6
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 22:54:20 -0600

    Try dir scripting.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+set -euo pipefail
 
-mkdir -p artifacts/install
+SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+echo $SCRIPT_DIR
+
+ARTIFACT_DIR="$SCRIPT_DIR/../artifacts"
+echo $ARTIFACT_DIR
+
+mkdir -p "$ARTIFACT_DIR/install"
 curl -sSL https://dot.net/v1/dotnet-install.sh > artifacts/install/dotnet-install.sh
 chmod +x artifacts/install/dotnet-install.sh
 ./artifacts/install/dotnet-install.sh -c 10.0 -InstallDir artifacts/dotnet
 
 ./artifacts/dotnet/dotnet --version
-./artifacts/dotnet/dotnet publish --configuration Release --property:PublishDir="./artifacts/releases"
+./artifacts/dotnet/dotnet publish --configuration Release --property:PublishDir="$ARTIFACT_DIR/releases"
