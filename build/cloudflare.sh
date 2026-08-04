61b9886b62fabba87fb51099ad7067d3e32440ff
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 23:28:41 -0600

    Debug lines.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd "$( dirname "$BASH_SOURCE[0]" )" && pwd)
+echo $SCRIPT_DIR
 REPO_DIR=$( dirname "$SCRIPT_DIR" )
+echo $REPO_DIR
 ARTIFACT_DIR="$REPO_DIR/artifacts"
+echo $ARTIFACT_DIR
 
 mkdir -p artifacts/install
 curl -sSL https://dot.net/v1/dotnet-install.sh > artifacts/install/dotnet-install.sh
