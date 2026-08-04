cdafca0e2993f80956cb6f647345afaba3c9da3f
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 23:16:48 -0600

    Try this.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -8,8 +8,8 @@ ARTIFACT_DIR="$REPO_DIR/artifacts"
 echo $ARTIFACT_DIR
 
 mkdir -p "$ARTIFACT_DIR/install"
-curl -sSL https://dot.net/v1/dotnet-install.sh > $ARTIFACT_DIR/install/dotnet-install.sh
-chmod +x $ARTIFACT_DIR/install/dotnet-install.sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > "$ARTIFACT_DIR/install/dotnet-install.sh"
+chmod +x "$ARTIFACT_DIR/install/dotnet-install.sh"
 . "$ARTIFACT_DIR/install/dotnet-install.sh" -c 10.0 -InstallDir $ARTIFACT_DIR/dotnet
 
 . "$ARTIFACT_DIR/dotnet/dotnet" --version
