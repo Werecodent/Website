7af0c7de28eb1acb6f608cf6400658615e2a7077
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-08-03 23:13:56 -0600

    Fixes.

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+SCRIPT_DIR=$(cd "$( dirname "$BASH_SOURCE[0]" )" && pwd)
 echo $SCRIPT_DIR
 REPO_DIR=$( dirname "$SCRIPT_DIR" )
 echo $REPO_DIR
@@ -10,7 +10,7 @@ echo $ARTIFACT_DIR
 mkdir -p "$ARTIFACT_DIR/install"
 curl -sSL https://dot.net/v1/dotnet-install.sh > $ARTIFACT_DIR/install/dotnet-install.sh
 chmod +x $ARTIFACT_DIR/install/dotnet-install.sh
-./artifacts/install/dotnet-install.sh -c 10.0 -InstallDir $ARTIFACT_DIR/dotnet
+. "$ARTIFACT_DIR/install/dotnet-install.sh" -c 10.0 -InstallDir $ARTIFACT_DIR/dotnet
 
-./artifacts/dotnet/dotnet --version
-./artifacts/dotnet/dotnet publish --configuration Release --property:PublishDir="$ARTIFACT_DIR/releases"
+. "$ARTIFACT_DIR/dotnet/dotnet" --version
+. "$ARTIFACT_DIR/dotnet/dotnet" publish --configuration Release --property:PublishDir="$ARTIFACT_DIR/releases"
